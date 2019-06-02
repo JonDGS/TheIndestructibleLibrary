6aa12e6cbc7f807e117f95bbcb5f3f1968f120b1
Alvaro Vargas M <valvaro707@gmail.com>
2019-06-01 22:30:17 -0600

    Tested the Create a file method of file manager, all working according to expectations.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 #include "MetaDB.h"
+#include "RAID/FileManager.h"
 
 using namespace std;
 
 int main() {
+
+    FileManager fm;
+    fm.createFile("test1","/media/alvar/AVM/MyRAID5/Disk_A","THIS IS A TEST");
 
     cout<<"all good"<<endl;
 
