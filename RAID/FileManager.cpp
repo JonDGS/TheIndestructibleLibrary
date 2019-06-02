201ab3b7902932dc9b34bd1d996d72174bd59b69
Alvaro Vargas M <valvaro707@gmail.com>
2019-06-01 22:26:37 -0600

    Added some basic methods to the File Manager class, untested atm of this commit.

--- RAID/FileManager.cpp
+++ RAID/FileManager.cpp
@@ -3,3 +3,19 @@
 //
 
 #include "FileManager.h"
+
+void FileManager::createFile(std::string file_Name, std::string file_Location, std::string file_Data) {
+    std::string type = "txt";
+
+    std::ofstream file;
+
+    file.open(file_Location + "/" + file_Name + "." + type,std::ios::out);
+
+    file << file_Data;
+
+    file.close();
+}
+
+std::string FileManager::readRile(std::string file_Name, std::string file_Location) {
+    return std::__cxx11::string();
+}
--- RAID/FileManager.h
+++ RAID/FileManager.h
@@ -2,13 +2,16 @@
 // Created by alvar on 01/06/19.
 //
 
-#ifndef THEINDESTRUCTIBLELIBRARY_FILEMANAGER_H
-#define THEINDESTRUCTIBLELIBRARY_FILEMANAGER_H
+#pragma once
+
+#include <iostream>
+#include <fstream>
+#include <string>
 
 
 class FileManager {
-
+public:
+    FileManager() = default;
+    void createFile(std::string file_Name,std::string file_Location,std::string file_Data);
+    std::string readRile(std::string file_Name,std::string file_Location);
 };
-
-
-#endif //THEINDESTRUCTIBLELIBRARY_FILEMANAGER_H
